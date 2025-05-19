72a752c71e5a771a01fa3e1dffc5bd47a90864f8
fuzhenn <fuzhen@maptalks.org>
2025-05-19 16:42:09 +0800

    move GLTFLineString calculation to GLTFPack.arrangeAlongLine

--- packages/layer-gltf/src/GLTFLineString.js
+++ packages/layer-gltf/src/GLTFLineString.js
@@ -7,7 +7,8 @@ const options = {
     gapLength: 0,
     scaleVertex: true
 };
-const TEMP_VEC = [];
+
+const TEMP_COORD = new Coordinate(0, 0);
 
 export default class GLTFLineString extends MultiGLTFMarker {
     constructor(coordinates, options) {
@@ -53,9 +54,12 @@ export default class GLTFLineString extends MultiGLTFMarker {
             return;
         }
         this.removeAllData();
+        const map = this.getMap();
+        const scale = this._calGLTFScale();
         for (let i = 0; i < coordinates.length - 1; i++) {
             const from = this._toCoordinate(coordinates[i]), to = this._toCoordinate(coordinates[i + 1]);
-            const dataArr = this._generateData(from, to);
+            const projectionScale = this._calProjectionScale(from, to);
+            const dataArr = this.gltfPack.arrangeAlongLine(map, from, to, scale, projectionScale, this.options);
             dataArr.forEach(item => {
                 this.addData(item);
             });
@@ -63,66 +67,7 @@ export default class GLTFLineString extends MultiGLTFMarker {
         this._dirty = true;
     }
 
-    _generateData(from, to) {
-        const items = [];
-        const map = this.getMap();
-        if (!map || !this.isLoaded()) {
-            return items;
-        }
-        const dist = map.getProjection().measureLenBetween(from, to);
-        const boxWidth = this._calBoxWidth(from, to);
-        const times = Math.floor(dist / boxWidth);
-        const rotationZ = this._getRotation(from, to);
-        //取余缩放
-        if (times >= 1) {
-            for (let i = 1; i <= times; i++) {
-                const t = boxWidth * (i - 0.5) / dist;
-                const item = {
-                    coordinates: this._calItemCenter(from, to, t),
-                    scale: [1, 1, 1],
-                    rotation: [0, 0, rotationZ]
-                }
-                items.push(item);
-            }
-            //尾巴
-            if (this.options['scaleVertex']) {
-                const t = (boxWidth * times + (dist - boxWidth * times) / 2) / dist;
-                const scale = (dist - boxWidth * times) / boxWidth;
-                const item = {
-                    coordinates: this._calItemCenter(from, to, t),
-                    scale: [scale, 1, 1],
-                    rotation: [0, 0, rotationZ]
-                }
-                items.push(item);
-            }
-        } else if (this.options['scaleVertex']) {
-            const scale = dist / boxWidth;
-            const item = {
-                coordinates: this._calItemCenter(from, to, 0.5),
-                scale: [scale, 1, 1],
-                rotation: [0, 0, rotationZ]
-            }
-            items.push(item);
-        }
-        return items;
-    }
-
-    _calItemCenter(from, to, t) {
-        const x = lerp(from.x, to.x, t);
-        const y = lerp(from.y, to.y, t);
-        const z1 = from.z || 0;
-        const z2 = to.z || 0;
-        const z = lerp(z1, z2, t);
-        return new Coordinate(x, y, z);
-    }
-
-    _calBoxWidth(from, to) {
-        const map = this.getMap();
-        const glRes = map.getGLRes();
-        const center = new Coordinate(0, (from.y + to.y) / 2);
-        const ratio = map.altitudeToPoint(100, glRes, center) / map.altitudeToPoint(100, glRes);
-        const gltfmodelBBox = this.getGLTFBBox();
-        const direction = this.options.direction || 0;
+    _calGLTFScale() {
         const scale = [1, 1, 1];
         const modelHeight = this.getModelHeight();
         if (modelHeight) {
@@ -131,22 +76,15 @@ export default class GLTFLineString extends MultiGLTFMarker {
             const symbol = this.getSymbol();
             vec3.set(scale, symbol.scaleX || 1, symbol.scaleY || 1, symbol.scaleZ || 1);
         }
-        const boxExtent = vec3.sub(TEMP_VEC, gltfmodelBBox.max, gltfmodelBBox.min);
-        vec3.multiply(boxExtent, boxExtent, scale);
-        const gapLength = this.options['gapLength'];
-        return boxExtent[direction] / ratio + gapLength;
+        return scale;
     }
 
-    _getRotation(from, to) {
+    _calProjectionScale(from, to) {
         const map = this.getMap();
-        const res = map.getGLRes();
-        const vp = map.coordinateToPointAtRes(from, res);
-        const vp1 = map.coordinateToPointAtRes(to, res);
-        const degree = Util.computeDegree(
-            vp1.x, vp1.y,
-            vp.x, vp.y
-        );
-        return degree / Math.PI * 180;
+        const glRes = map.getGLRes();
+        TEMP_COORD.set(0, (from.y + to.y) / 2);
+        const ratio = map.altitudeToPoint(100, glRes, TEMP_COORD) / map.altitudeToPoint(100, glRes);
+        return ratio;
     }
 
     _toCoordinate(coord) {
@@ -155,10 +93,6 @@ export default class GLTFLineString extends MultiGLTFMarker {
         }
         return coord;
     }
-}
-
-function lerp(a, b, t) {
-    return a + t * (b - a);
 }
 
 GLTFLineString.mergeOptions(options);
