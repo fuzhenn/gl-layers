07de26e9c4072f77ee399716fb67f5c4439de120
fuzhenn <fuzhen@maptalks.org>
2025-06-19 17:17:51 +0800

    only enable stencil on 2d data, and more detailed error message when failing to read vt pbf

--- packages/vt/src/layer/plugins/painters/FillPainter.js
+++ packages/vt/src/layer/plugins/painters/FillPainter.js
@@ -533,8 +533,8 @@ class FillPainter extends BasicPainter {
         const extraCommandProps = {
             viewport,
             stencil: {
-                enable: () => {
-                    return this.isEnableTileStencil(context);
+                enable: (_, props) => {
+                    return props.geometryProperties.is2D && this.isEnableTileStencil(context);
                 },
                 func: {
                     cmp: () => {
